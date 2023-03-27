f1e7cea2f63f453e33c1a6d9380cfef838a62334
Gleb Tregubov <tregubovgleb@gmail.com>
2023-03-27 15:13:32 +0300

    done

--- task9/ViewController.swift
+++ task9/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Collection"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 20
+        layout.sectionInset = .init(top: .zero, left: 20, bottom: .zero, right: 20)
+        
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: String(describing: UICollectionViewCell.self))
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        view.addSubview(collectionView)
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
     }
 
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { 13 }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: String(describing: UICollectionViewCell.self),
+            for: indexPath
+        )
+        cell.backgroundColor = .lightGray
+        cell.layer.cornerRadius = 20
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 450)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let itemWidth = CGFloat(300)
+
+        let cellWidth = itemWidth + 20
+        let targetXContentOffset = targetContentOffset.pointee.x / cellWidth
+
+        let offset = round(targetXContentOffset) * cellWidth - 20
+        targetContentOffset.pointee.x = offset
+    }
+    
 }
 
